63706295436bf8c24b815660092510827e2f8d72
Izaac Abrams <izaac.abrams25@gmail.com>
2020-01-15 17:42:50 -0600

    added close button to close current selection

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ function displaySearch(responseJson){
     for(let i=0; i < responseJson.results.length; i++){
         $('.ul-results').append(
             `<li class="result-li item${i}" id="${responseJson.results[i].id}">
+            <span class="close hidden">&times;</span>
             <img src="https://image.tmdb.org/t/p/original${responseJson.results[i].poster_path}">
             <div class="title-content"><section class="title-details hidden">
             <h2 class="list-title">${responseJson.results[i].original_title}</h2>
@@ -67,24 +68,28 @@ function watchResults(){
         if($('.result-li').hasClass('selected') === true){
             $('.result-li').removeClass('selected');
             $('.title-details').addClass('hidden');
-            $(this).addClass('selected modal-content');
-            $(this).wrap('<div class="modal"></div>')
-            $(this).addClass('modal-content');
-            $(this).prepend('<span class="close">&times;</span>');
-            $('.title-details', this).removeClass('hidden');
-            getColors();
+            $('.close').addClass('hidden');
+            $('.selected > .title-content > .title-details').removeClass('hidden');
+            $('.selected > .close').removeClass('hidden')
+            // getColors();
             
         }else{
             $(this).addClass('selected modal-content');
-            $(this).wrap('<div class="modal"></div>')
-            $(this).prepend('<span class="close">&times;</span>');
-            $('.title-details', this).removeClass('hidden');
-            getColors();
+            $('.selected > .title-content > .title-details').removeClass('hidden')
+            $(this).wrap('<div class="modal"></div>');
+            $('.selected > .close').removeClass('hidden');
+            // getColors();
         }
         const titleId = $(this).attr('id');
         $('.similar-submit').on('click', function(e){
           e.preventDefault();
           getSimilar(titleId);  
+        })
+        $('.close').on('click', function(e){
+            e.preventDefault();
+            $('.selected').parent('div').removeClass('modal');
+            $('.selected').removeClass('modal-content');
+            console.log(this)
         })
     })
 }
@@ -105,31 +110,31 @@ function getSimilar(titleId){
     .then(responseJson => displaySearch(responseJson));
 }
 
-function getColors() {
-    const options = {
-            headers: new Headers({
-                'x-rapidapi-key': rapidApiKey,
-                'X-rapidapi-host': "apicloud-colortag.p.rapidapi.com"})
-        };
-        const url = 'https://apicloud-colortag.p.rapidapi.com/tag-url.json';
-        const currrentImgUrl = $('.selected > img').attr('src');
-        const params = {
-            'url': currrentImgUrl,
-            'palette': 'precise'
-        }
-        const fullUrl = url + '?' + makeFullUrl(params);
-        fetch(fullUrl, options)
-        .then(response => response.json())
-        .then(responseJson => showColors(responseJson));
-}
+// function getColors() {
+//     const options = {
+//             headers: new Headers({
+//                 'x-rapidapi-key': rapidApiKey,
+//                 'X-rapidapi-host': "apicloud-colortag.p.rapidapi.com"})
+//         };
+//         const url = 'https://apicloud-colortag.p.rapidapi.com/tag-url.json';
+//         const currrentImgUrl = $('.selected > img').attr('src');
+//         const params = {
+//             'url': currrentImgUrl,
+//             'palette': 'precise'
+//         }
+//         const fullUrl = url + '?' + makeFullUrl(params);
+//         fetch(fullUrl, options)
+//         .then(response => response.json())
+//         .then(responseJson => showColors(responseJson));
+// }
 
-function showColors(responseJson){
-    console.log(responseJson)
-    $('.color-palette').empty();
-    for(let i=0; i < 6; i++)
-    $('.selected > .title-content > .color-palette').append(`
-    <div class="color-palette-item" style="background-color:${responseJson.tags[i].color}"></div>`)
-}
+// function showColors(responseJson){
+//     console.log(responseJson)
+//     $('.color-palette').empty();
+//     for(let i=0; i < 6; i++)
+//     $('.selected > .title-content > .color-palette').append(`
+//     <div class="color-palette-item" style="background-color:${responseJson.tags[i].color}"></div>`)
+// }
 
 
 $(watchForm(), 
